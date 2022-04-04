74026967f89c802e47cf579b58e43dc03c06910f
Štěpán Šubrt <68623415+subinho@users.noreply.github.com>
2022-04-04 23:44:15 +0200

    added app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+var myAge = 21; // my age
+let earlyYears = 2; // idk yet
+earlyYears = earlyYears * 10.5; 
+let laterYears = myAge / 2; // substructing myAge
+laterYears *= 4; // multiplying laterYears
+console.log(earlyYears + ' ' + laterYears)
+var myAgeInDogYears = earlyYears + laterYears; // my age in dog years
+var myName = 'Stepan'.toLowerCase(); // my name
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`) // description
